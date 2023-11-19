0368abf48080d296ca4a22f63e3a22fc432f6564
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2023-11-19 14:15:23 -0500

    further tailwind configu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+
+      },
+    },
   },
   plugins: [],
 }
